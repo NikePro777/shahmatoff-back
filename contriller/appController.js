a907409c3f3f53ddde623eec85786fef86d60e97
Evgeniy Datskij <evgenij.datskij@mail.ru>
2023-03-10 23:01:07 +0500

    test error

--- contriller/appController.js
+++ contriller/appController.js
@@ -1,4 +1,35 @@
-const signup = (req, res) => {
+const nodemailer = require("nodemailer");
+
+const signup = async (req, res) => {
+  let testAccount = await nodemailer.createTestAccount();
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
+  let message = {
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "bar@example.com, baz@example.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  };
+
+  transporter
+    .sendMail(message)
+    .then(() => {
+      return res.status(201).json({ msg: "you message send to email" });
+    })
+    .catch((error) => {
+      return res.status(500).json({ error });
+    });
+
   res.status(201).json("Signup Successfully...!");
 };
 
